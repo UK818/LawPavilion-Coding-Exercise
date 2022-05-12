1321359b08513af84ff6d08db53491d5b00d4448
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-05-12 14:35:48 +0100

    Initial Commit

---
+++ LawPavilion Coding Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LawPavilion Coding Exercise
+//
+//  Created by mac on 12/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
